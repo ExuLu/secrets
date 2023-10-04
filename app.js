e936daacdd42f9f1399a555cde6f863180448e7b
Alena Sharai <exultantislupus@gmail.com>
2023-10-04 16:45:12 +0400

    add environment vars

---
+++ app.js
@@ -0,0 +1,102 @@
+//jshint esversion:6
+import 'dotenv/config';
+import express from 'express';
+import bodyParser from 'body-parser';
+import ejs from 'ejs';
+import mongoose from 'mongoose';
+import encrypt from 'mongoose-encryption';
+
+const app = express();
+const uri = process.env.MONGODB_SECRET_URI;
+
+app.use(express.static('public'));
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({ extended: true }));
+
+main().catch((err) => console.log(err));
+
+async function main() {
+  await mongoose.connect(`${uri}/userDB`, {
+    serverSelectionTimeoutMS: 5000,
+  });
+}
+
+const userSchema = new mongoose.Schema({
+  email: String,
+  password: String,
+});
+
+const secret = process.env.SECRET;
+userSchema.plugin(encrypt, {
+  secret: secret,
+  encryptedFields: ['password'],
+});
+
+const User = new mongoose.model('User', userSchema);
+
+app.get('/', function (req, res) {
+  res.render('home');
+});
+
+app.get('/login', function (req, res) {
+  res.render('login');
+});
+
+app.get('/register', function (req, res) {
+  res.render('register');
+});
+
+app.get('/logout', function (req, res) {
+  res.redirect('/');
+});
+
+app.post('/login', async function (req, res) {
+  const username = req.body.username;
+  const password = req.body.password;
+  const foundUser = await User.findOne({
+    email: username,
+  });
+
+  if (foundUser) {
+    if (foundUser.password === password) {
+      console.log(`You've successfully loged in`);
+      res.render('secrets');
+    } else {
+      console.log(`Wrong password!`);
+      res.redirect('/login');
+    }
+  } else {
+    console.log('You need to register your user first!');
+    res.redirect('/login');
+  }
+});
+
+app.post('/register', async function (req, res) {
+  const username = req.body.username;
+  const password = req.body.password;
+  const foundUser = await User.findOne({
+    email: username,
+  });
+
+  if (!foundUser) {
+    if (!username || !password) {
+      console.log(`You don't have username or password`);
+      res.redirect('/register');
+    } else {
+      const user = new User({
+        email: username,
+        password: password,
+      });
+      await user.save();
+      console.log(`You've successfully registered`);
+      res.render('secrets');
+    }
+  } else {
+    console.log(`You've already registered`);
+    res.redirect('/');
+  }
+});
+
+app.listen(3000, () => {
+  console.log('Server started on port 3000');
+});
